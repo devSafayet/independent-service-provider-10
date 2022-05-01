19d365976f3a1cb31195c5e34ed5b4b0796e7fc5
Safayet <mrishafayet003@gmail.com>
2022-05-01 13:51:49 +0600

    added loading spinner

--- src/Components/Shared/Loading/Loading.js
+++ src/Components/Shared/Loading/Loading.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 
 const Loading = () => {
     return (
-        <div>
-
+        <div style={{ height: "450px", width: "100%", textAlign: "center" }}>
+            <Spinner animation="border" variant="primary" />
         </div>
     );
 };
