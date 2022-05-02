078036ba6d056ec90da2a1a8a7c419470e6c3097
Safayet <mrishafayet003@gmail.com>
2022-05-03 00:25:31 +0600

    responsive all done

--- src/Components/Home/AppointSuccess/AppointSuccess.js
+++ src/Components/Home/AppointSuccess/AppointSuccess.js
@@ -4,7 +4,7 @@ import './ApointSuccess.css';
 const AppointSuccess = () => {
 
     return (
-        <div className='text-success successapoint '>
+        <div className='text-success successapoint'>
             <h1 className='pt-5'>Thank You. This is Submitted.</h1>
             <img src="https://media.istockphoto.com/photos/painted-hand-shows-concept-hologram-thank-you-on-his-hand-picture-id963330932?k=20&m=963330932&s=612x612&w=0&h=hdJMLxWViuFAIeg5RNbvCaIm_TBF20mcbkEdxacn0RI=" alt="" />
 
